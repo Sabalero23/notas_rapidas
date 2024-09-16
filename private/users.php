532780c61e21a0b005dee8c0144855bba4fd2e6b
Paloma <67202783+Sabalero23@users.noreply.github.com>
2024-09-15 23:03:52 -0300

    Update users.php

--- private/users.php
+++ private/users.php
@@ -1,13 +1,8 @@
 <?php
 return array (
-  'c33367701511b4f6020ec61ded352059' => 
-  array (
-    'username' => 'Martin',
-    'role' => 'user',
-  ),
   'e10adc3949ba59abbe56e057f20f883e' => 
   array (
-    'username' => 'Paloma',
+    'username' => 'Admin',
     'role' => 'admin',
   ),
 );
